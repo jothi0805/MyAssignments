9d9710e1ecf8991a6e622ece8323a06202d77641
jothi0805 <jothialagumalai21@gmail.com>
2025-08-06 18:35:51 +0530

    Add files via upload
    Day1 Assignment completed

---
+++ testleaf/Chrome.java
@@ -0,0 +1,19 @@
+package datatypescoding.testleaf;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version = 91.0f;
+		String developer = "Google";
+		boolean beta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'c';
+
+		System.out.println("my version is " + version + "\n" + "developer is " + developer + "\n" + "beta is " + beta
+				+ "\n" + "releaseYear " + releaseYear+ "\n"+"short Cut Key "+ shortCutKey);
+
+	}
+
+}
