92c66985d74872cb0ff7c12e6e8f512730d1766c
jothi0805 <jothialagumalai21@gmail.com>
2025-08-11 20:13:26 +0530

    Add files via upload

---
+++ testleaf/code/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package seleniumprogram.code;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+
+		Browser bro1 = new Browser();
+		bro1.launchBrowser(null);
+		bro1.loadUrl();
+	}
+
+}
